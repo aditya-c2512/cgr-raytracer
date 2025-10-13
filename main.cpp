1c7e47fc8839f99c3e86b0e5689c67d94d272594
aditya-c2512 <adityachoubey2512@gmail.com>
2025-10-13 17:58:49 +0100

    Added help function

--- main.cpp
+++ main.cpp
@@ -3,6 +3,28 @@
 #include <utils/args-parser.h>
 #include <raytracer_app.h>
 
+void printHelp() {
+    std::cout << "\n=== Command Line Arguments Help ===\n";
+    std::cout << std::left << std::setw(20) << "Argument"
+              << std::setw(12) << "Required"
+              << "Description\n";
+    std::cout << std::string(60, '-') << "\n";
+
+    std::cout << std::left << std::setw(20) << "--help"
+                  << std::setw(12) << "No"
+                  << "Get help on application usage and argument descriptions." << "\n";
+    std::cout << std::left << std::setw(20) << "--scene"
+                  << std::setw(12) << "Yes"
+                  << "Provide the absolute or relative path of a JSON object describing the scene to be rendered." << "\n";
+    std::cout << std::left << std::setw(20) << "--render-output"
+                  << std::setw(12) << "No"
+                  << "Provide the absolute or relative path to where the rendered image should be written to." << "\n";
+
+    std::cout << "\nExample usage:\n"
+              << "  ./program --scene=\"/my/input/path/scene.json\" --render-output=\"/my/output/path/render.ppm\"\n"
+              << std::endl;
+}
+
 int main(int argc, char** argv) {
     Logger* logger = Logger::getInstance();
     logger->setLogLevel(LOG_LEVEL_INFO);
@@ -10,7 +32,7 @@ int main(int argc, char** argv) {
     auto* args = new ArgsParser(argc, argv);
 
     if (args->get("help").boolVal) {
-        logger->info("Usage: raytracer [OPTIONS]");
+        printHelp();
         return EXIT_SUCCESS;
     }
 
